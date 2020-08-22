ceb0c9e7de89b4fac44041a0f55033a8956d0101
Panagiotis Kanakakis <takiskanakakis@lstech.io>
2020-08-22 11:26:00 +0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,20 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
-import { HomeComponent } from '@src/app/home/home.component';
+import {HomeComponent} from '@src/app/home/home.component';
+import {CharacterSelectionComponent} from '@src/app/character-selection/character-selection.component';
 
 export const routes: Routes = [
-  {
-      path: '',
-      redirectTo: '/home',
-      pathMatch: 'full',
-  },
-  {
-      path: 'home',
-      component: HomeComponent,
-  },
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full',
+    },
+    {
+        path: 'home',
+        component: HomeComponent,
+    },
+    {
+        path: 'character-selection',
+        component: CharacterSelectionComponent,
+    }
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {orientationCleanup, setCurrentOrientation} from 'nativescript-screen-orientation';
+import {GestureEventData, Page} from '@nativescript/core';
+import * as fs from 'tns-core-modules/file-system';
+import {NavigationExtras, Router} from '@angular/router';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  title = 'childrens-first-cross-platform';
+    public bgSrc: string;
+    public logoSrc: string;
+    private router:Router;
+    private REDIRECT_ROUTE = ['/character-selection'];
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+    constructor(router: Router, page: Page) {
+        this.router = router;
+        this.bgSrc = encodeURI(`${fs.knownFolders.currentApp().path}/assets/images/background.PNG`);
+        this.logoSrc = encodeURI(`${fs.knownFolders.currentApp().path}/assets/images/logo.PNG`);
+        // page.actionBarHidden = true;
+        page.on('navigatedTo', function() {
+            setCurrentOrientation('landscape', function() {
+                console.log('landscape orientation');
+            });
+        });
+        page.on('navigatingFrom', function() {
+            orientationCleanup();
+        });
+    }
+
+    onTap(args: GestureEventData) {
+        this.router.navigate(this.REDIRECT_ROUTE, {clearHistory: true} as NavigationExtras);
+    }
+
+    ngOnInit() {
+    }
 }
